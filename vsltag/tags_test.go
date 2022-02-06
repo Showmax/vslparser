3925b693425906f4f78ee4fac8910c2b92a8f8f7
Jan Dubsky <jan.dubsky@showmax.com>
2022-02-06 22:05:48 +0100

    [vsltag] Correctly propagate errors

--- vsltag/tags_test.go
+++ vsltag/tags_test.go
@@ -1,12 +1,12 @@
 package vsltag_test
 
 import (
-	"bytes"
-	vsl "github.com/Showmax/vslparser"
-	"github.com/Showmax/vslparser/vsltag"
 	"net"
 	"testing"
 	"time"
+
+	vsl "github.com/Showmax/vslparser"
+	"github.com/Showmax/vslparser/vsltag"
 )
 
 func TestSessOpen(t *testing.T) {
@@ -59,7 +59,7 @@ func TestSessOpen(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			sessOpen := vsltag.SessOpen(tt.tag)
 
-			if gotAddr, gotPort := sessOpen.RemoteAddr(); !bytes.Equal(gotAddr, tt.want.remoteIP) || gotPort != tt.want.remotePort {
+			if gotAddr, gotPort := sessOpen.RemoteAddr(); !gotAddr.Equal(tt.want.remoteIP) || gotPort != tt.want.remotePort {
 				t.Errorf("RemoteAddr() = %v:%v, want %v:%v",
 					gotAddr, gotPort,
 					tt.want.remoteIP, tt.want.remotePort)
@@ -67,15 +67,15 @@ func TestSessOpen(t *testing.T) {
 			if got := sessOpen.SocketName(); got != tt.want.socketName {
 				t.Errorf("SocketName() = %v, want %v", got, tt.want.socketName)
 			}
-			if gotAddr, gotPort := sessOpen.LocalAddr(); !bytes.Equal(gotAddr, tt.want.localIP) || gotPort != tt.want.localPort {
+			if gotAddr, gotPort := sessOpen.LocalAddr(); !gotAddr.Equal(tt.want.localIP) || gotPort != tt.want.localPort {
 				t.Errorf("LocalAddr() = %v:%v, want %v:%v",
 					gotAddr, gotPort,
 					tt.want.localIP, tt.want.localPort)
 			}
-			if got := sessOpen.SessionStart(); !got.Equal(tt.want.sessStrat) {
-				t.Errorf("SessionStart() = %v, want %v",
+			if got, err := sessOpen.SessionStart(); err != nil || !got.Equal(tt.want.sessStrat) {
+				t.Errorf("SessionStart() = %v, want %v (error: %v)",
 					// Show it in UTC to be it easy to compare for humans.
-					got.In(time.UTC), tt.want.sessStrat.In(time.UTC))
+					got.In(time.UTC), tt.want.sessStrat.In(time.UTC), err)
 			}
 			if got := sessOpen.FileDescriptor(); got != tt.want.fileDesc {
 				t.Errorf("FileDescriptor() = %v, want %v", got, tt.want.fileDesc)
