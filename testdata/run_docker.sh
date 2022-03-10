c7914ad76232444d756dcd7b20cdf88a0e6f9bfc
Jan Dubsky <jan.dubsky@showmax.com>
2022-03-10 11:56:40 +0100

    CR

--- testdata/run_docker.sh
+++ testdata/run_docker.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+set -eu
 
 # The purpose of this script is to build the docker image and to run it as an
 # interactive shell with `testdata` directory (the dir this script it located
 # in) mounted in the container as /output directory.
+#
+# As the docker image is supposed to simplify collecting varnishlog varnishlog
+# outputs for test purposes, we need to mount local testdata directory into the
+# container. That's the main purpose of this script.
+
+cd -- "$(dirname -- "$0")"
 
 readonly image_name="${1:varnish}"
 
